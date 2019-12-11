fd665fbc8b62690da97f296ee99b776442d2ad2c
Nick <NickBasso@users.noreply.github.com>
2019-12-11 19:25:33 +0200

    3B GOOD

--- Solutions/_Proiect4/3B/Solution.cpp
+++ Solutions/_Proiect4/3B/Solution.cpp
@@ -11,19 +11,16 @@ int main(){
 	
 	int a[n+1] = {0};
 	
-	for(i = 0; i < n; i++){
+	for(i = 0; i < n; i++)
 		cin >> a[i];
-	}
-	
-	int highest = 0;
+
+	int last_predecessor = a[n - 1], pos = n - 1;
 	for(i = n - 2; i >= 0; i--){
-		if(a[i + 1] > highest){
-			highest = a[i + 1];
-		}
-		
-		if(highest < a[i]){
+		if(last_predecessor < a[i])
 			ans++;
-		}
+			
+		pos = i;
+		last_predecessor = a[i];
 	}
 	
 	cout << ans;
